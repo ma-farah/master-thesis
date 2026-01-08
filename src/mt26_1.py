74189d8ddd0aac1a60e2012f6420d0ebd6970df0
munafarah <muna.ahmed.farah@nmbu.no>
2026-01-08 02:52:07 +0100

    exploring dataset

--- src/mt26_1.py
+++ src/mt26_1.py
@@ -1,1 +1,39 @@
 # first data-loading
+#%%
+import pandas as pd
+from pathlib import Path
+
+data_dir = Path(__file__).resolve().parents[1] / "data"
+data = data_dir / "LDRT_raw.xlsx"
+
+#%%
+
+# immunological data, columns starts at row 5
+df_im = pd.read_excel(
+    data,
+    sheet_name="IPT ",
+    header=4,
+    engine="openpyxl"
+)
+
+# Clinical data, columns starts at row 2
+df_cl = pd.read_excel(
+    data,
+    sheet_name="Patient data & Pain",
+    header=1,
+    engine="openpyxl"
+)
+
+
+# %%
+from skrub import TableReport
+
+# clinical dataset
+TableReport(df_cl, max_plot_columns=138)
+
+#%%
+
+# immunological dataset
+TableReport(df_im, max_plot_columns=138)
+
+
