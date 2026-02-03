7bf6c1f060a4a40da95f7e9b86fd3c33aa41d8fb
munafarah <muna.ahmed.farah@nmbu.no>
2026-02-03 04:02:14 +0100

    added files

--- src/exploratory.py
+++ src/exploratory.py
@@ -1,1 +1,12 @@
-#hi
+# imports
+import pandas as pd
+from pathlib import Path
+from skrub import TableReport
+import scikit_na as na
+import numpy as np
+import hoggorm as ho
+import prince as ps
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
